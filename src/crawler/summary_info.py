4fb48d3879d3ba504450de4f27bdd2bd1065aa37
Jeonghun-Ban <bjh5230@gmail.com>
2020-02-15 04:46:32 +0900

    Add summary_info support

--- src/crawler/summary_info.py
+++ src/crawler/summary_info.py
@@ -4,16 +4,16 @@ from bs4 import BeautifulSoup
 import json
 
 # 확진자 수
-confirmator_num = 0
+confirmator_num = ''
 # 퇴원조치 수
-discharged_num = 0
+discharged_num = ''
 # 검사진행 수
-check_num = 0
+check_num = ''
 
 #dict
 summary = {}
 
-# 지역별 확진자(해야됌)
+# TODO 지역별 확진자
 
 # retry get raw without timeout exception
 def get_raw(url):
@@ -23,21 +23,18 @@ def get_raw(url):
         except:
             pass
 
-raw_status = get_raw("http://ncov.mohw.go.kr/index_main.jsp#link")
-html_status = BeautifulSoup(raw_status.text, 'html.parser')
+def get_json():
+    raw_status = get_raw("http://ncov.mohw.go.kr/index_main.jsp#link")
+    html_status = BeautifulSoup(raw_status.text, 'html.parser')
 
-statusbox = html_status.select("div.co_cur a.num")
-confirmator_num = int(statusbox[0].text[:-2])
-discharged_num = int(statusbox[1].text[:-2])
-check_num = int(statusbox[2].text[:-2])
+    statusbox = html_status.select("div.co_cur a.num")
+    confirmator_num = int(statusbox[0].text[:-2])
+    discharged_num = int(statusbox[1].text[:-2])
+    check_num = int(statusbox[2].text[:-2])
 
-# dict to json
-summary["confirmator_num"] = confirmator_num
-summary["discharged_num"] = discharged_num
-summary["check_num"] = check_num
-# json_summary = json.dumps(summary, indent=4)
-print(summary)
+    # dict to json
+    summary["confirmator_num"] = confirmator_num
+    summary["discharged_num"] = discharged_num
+    summary["check_num"] = check_num
 
-# make jason file
-with open('summary.json', 'w', encoding="utf-8") as make_summary:
-    json.dump(summary, make_summary, ensure_ascii=False, indent="\t")
+    return json.dumps(summary, indent=4)
--- src/webhook.py
+++ src/webhook.py
@@ -1,6 +1,8 @@
 import json, os
 import requests
 
+from crawler import summary_info
+
 CHATBOT_RESPONSE = {
     '코로나': """바이러스""",
     '바이러스': """코로나""",
@@ -24,14 +26,28 @@ def lambda_handler(event, context):
         send_facebook_message(message['sender']['id'], message['message']['text'])
         return {'statusCode': '200', 'body': 'Success' , 'headers': {'Content-Type': 'application/json'}}
         
-def send_facebook_message(fbid, recevied_message):
+def send_facebook_message(fbid, received_message):
     msg = ''
-    for key in CHATBOT_RESPONSE.keys():
-        if key in recevied_message:
-            msg += CHATBOT_RESPONSE[key] + "\n"
+
+    if '확진환자수' in received_message:
+        summary = json.loads(summary_info.get_json())
+        msg = summary["confirmator_num"]
+    
+    elif '퇴원조치수' in received_message:
+        summary = json.loads(summary_info.get_json())
+        msg = summary["discharged_num"]
+        
+    elif '검사진행수' in received_message:
+        summary = json.loads(summary_info.get_json())
+        msg = summary["check_num"]
+    
+    else:
+        for key in CHATBOT_RESPONSE.keys():
+            if key in received_message:
+                msg += key + ": " + CHATBOT_RESPONSE[key] + "\n"
 
     if not msg:
-        msg = "안녕하세요,\n코로나 알리미입니다!\n\n아래 제시된 키워드를 포함하여 질문해주세요.\n\n- 코로나\n- 바이러스"
+        msg = "안녕하세요,\n코로나 알리미입니다!\n\n아래 제시된 키워드를 포함하여 질문해주세요.\n\n- 확진환자수\n- 퇴원조치수\n- 검사진행수"
         
     endpoint = 'https://graph.facebook.com/v2.6/me/messages?access_token=' + os.environ['PAGE_ACCESS_TOKEN']
     response_msg = json.dumps({"recipient": {"id": fbid}, "message": {"text": msg}})
