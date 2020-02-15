f97c4434a2163febd1c6fa0d680a923a46a3e449
Jeonghun-Ban <bjh5230@gmail.com>
2020-02-15 16:04:14 +0900

    Add response messages

--- src/webhook.py
+++ src/webhook.py
@@ -2,10 +2,17 @@ import json, os
 import requests
 
 from crawler import summary_info
+from crawler import hospital_info
 
 CHATBOT_RESPONSE = {
-    '코로나': """바이러스""",
-    '바이러스': """코로나""",
+    '발단': """2019년 12월, 중국 우한에서 처음 발생했습니다. 감염원은 동물로 추정되고 있으며, 동물에게서 사람으로 전파된 것으로 추정됩니다.""",
+    '증상': """감염되면 최대 2주간의 잠복기를 거친 후, 발열 / 기침 / 호흡곤란을 비롯한 폐렴 증상이 주로 나타납니다. 다만, 증상이 나타나지 않는 무증상 감염 사례도 존재합니다.""",
+    '전염경로': """코로나19는 사람 간 전파가 확인된 바이러스입니다. 주된 감염경로는 비말감염으로, 감염자의 침방울이 호흡기나 눈/코/입의 점막으로 침투될 때 전염됩니다.""",
+    '예방법': """1. 우선, 비누와 물로 손을 자주 씻습니다. 손 소독제 사용도 좋은 대안입니다.\n
+    2. 씻지 않은 손으로 눈이나 코, 입을 만지지 않습니다.\n
+    3. 기침이나 재채기를 할 때 티슈나 소매로 입/코를 가립니다.\n
+    4. 아플 때는 자가격리를 통해 다른 사람과의 접촉을 피합니다.\n""",
+    '치료': """코로나19 치료는 환자의 증상에 대응하는 치료로 이루어집니다. 기침/인후통/폐렴 등 주요 증상에 따라 항바이러스제나 항생제 투여가 해당됩니다.""",
 }
 
 def lambda_handler(event, context):
@@ -30,24 +37,20 @@ def send_facebook_message(fbid, received_message):
     msg = ''
 
     if '확진환자수' in received_message:
-        summary = json.loads(summary_info.get_json())
-        msg = "현재까지 집계된 확진환자수는 %s명 입니다." % summary["confirmator_num"]
+        data = json.loads(summary_info.get_json())
+        msg = "현재시각 기준, 코로나19 확진환자는 %s명, 검사가 진행중인 유증상자는 %s명입니다." % (data["confirmator_num"], data["check_num"])
     
     elif '퇴원조치수' in received_message:
-        summary = json.loads(summary_info.get_json())
-        msg = "현재까지 집계된 퇴원조치수는 %s명 입니다." % summary["discharged_num"]
-        
-    elif '검사진행수' in received_message:
-        summary = json.loads(summary_info.get_json())
-        msg = "현재까지 집계된 검사진행수는 %s명 입니다." % summary["check_num"]
-    
+        data = json.loads(summary_info.get_json())
+        msg = "현재시각 기준, %s명이 코로나19 확진 후 퇴원조치(격리해제) 되었습니다." % data["discharged_num"]
+                
     else:
         for key in CHATBOT_RESPONSE.keys():
             if key in received_message:
-                msg += key + ": " + CHATBOT_RESPONSE[key] + "\n"
+                msg += CHATBOT_RESPONSE[key] + "\n"
 
     if not msg:
-        msg = "안녕하세요,\n코로나 알리미입니다!\n\n아래 제시된 키워드를 포함하여 질문해주세요.\n\n- 확진환자수\n- 퇴원조치수\n- 검사진행수"
+        msg = "안녕하세요,\n코로나 알리미입니다!\n\n아래 제시된 키워드를 포함하여 질문해주세요.\n\n- 발단\n- 증상\n- 전염경로\n- 예방법\n- 치료\n- 확진환자수\n- 퇴원조치수"
         
     endpoint = 'https://graph.facebook.com/v2.6/me/messages?access_token=' + os.environ['PAGE_ACCESS_TOKEN']
     response_msg = json.dumps({"recipient": {"id": fbid}, "message": {"text": msg}})
