e29a1ce06160ebd4ffc0ebb1fdfb80d91c38db4e
Chloekkk <51353146+Chloekkk@users.noreply.github.com>
2020-02-17 17:51:33 +0900

    update hospital_info.py

--- src/crawler/hospital_info.py
+++ src/crawler/hospital_info.py
@@ -4,10 +4,11 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
+hosiptal = {}
 # 전체 선별진료소
 hosiptal_list = []
-# 검체채취가능 진료소
-possible_hospital=[]
+# 시군구 리스트
+region_list = []
 
 # retry get raw without timeout exception
 def get_raw(url):
@@ -17,23 +18,40 @@ def get_raw(url):
         except:
             pass
 
-def get_json():
-    raw_hospital = get_raw("http://www.mohw.go.kr/react/popup_200128.html")
-    # raw_hospital.encoding = None
-    html_hospital = BeautifulSoup(raw_hospital.content, 'html.parser', from_encoding='euc-kr')
-    hospitals = html_hospital.select("tbody.tb_center tr")
+# 검체채취가능 진료소
+possible_hospital=[]
 
-    # 546
-    for h in hospitals:
-        id = h.select_one("th").text
-        city = h.select_one("td:nth-of-type(1)").text
-        region = h.select_one("td:nth-of-type(2)").text
-        selected = h.select_one("td:nth-of-type(3)").text.replace("	","")
-        number = h.select_one("td:nth-of-type(4)").text.replace(",","/")
+raw_hospital = get_raw("http://www.mohw.go.kr/react/popup_200128.html")
+# print(raw_hospital.encoding)
+# raw_hospital.encoding = None
+html_hospital = BeautifulSoup(raw_hospital.content, 'html.parser', from_encoding='euc-kr')
+hospitals = html_hospital.select("tbody.tb_center tr")
 
-        if "*" in selected:
-            possible_hospital.append(selected)
-        hosiptal_list.append({"city":city, "region":region, "name":selected,"number":number})
+# 546
+for h in hospitals:
+    id = h.select_one("th").text
+    city = h.select_one("td:nth-of-type(1)").text
+    region = h.select_one("td:nth-of-type(2)").text
+    selected = h.select_one("td:nth-of-type(3)").text.replace("	","").replace("(검체채취 가능)", "")
+    number = h.select_one("td:nth-of-type(4)").text.replace(",","/")
 
-    hospital = {"all_hospital":hosiptal_list, "sampling_hospital":possible_hospital}
-    return json.dumps(hospital, indent=4)
+    region_list.append(region)
+    region_list = list(set(region_list))
+
+    if "*" in selected:
+        possible_hospital.append(selected)
+
+    print(id,city,region,selected,number)
+    hosiptal_list.append({"city":city, "region":region, "name":selected,"number":number})
+
+hospital = {"all_hospital":hosiptal_list, "sampling_hospital":possible_hospital}
+json_hospital = json.dumps(hospital, indent=4)
+# print(json_hospital)
+# print(type(json_hospital))
+# print(possible_hospital)
+
+print(region_list)
+print(len(region_list))
+
+with open('hospital.json', 'w', encoding="utf-8") as make_hosptial:
+    json.dump(hospital, make_hosptial, ensure_ascii=False, indent="\t")
