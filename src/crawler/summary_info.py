74f3821c390d13c7e4ac73a2217bd84b907d6260
Jeonghun-Ban <bjh5230@gmail.com>
2020-02-24 07:17:58 +0900

    Add update_date in json attribute

--- src/crawler/summary_info.py
+++ src/crawler/summary_info.py
@@ -13,8 +13,8 @@ def set_summary_info(event, context):
     html_status = BeautifulSoup(raw_status.text, 'html.parser')
     statusbox = html_status.select("ul.s_listin_dot li")
     updatebox = html_status.select_one("p.s_descript").text
-    update_date = updatebox.replace("코로나바이러스감염증-19 국내 발생 현황", "")
-
+    
+    data['update_date'] = updatebox.replace("코로나바이러스감염증-19 국내 발생 현황", "")
     data['confirmator_num'] = statusbox[0].text[:-1].replace("(확진환자) ", "")
     data['discharged_num'] = statusbox[1].text[:-1].replace("(확진환자 격리해제) ", "")
     data['death_num'] = statusbox[2].text[:-1].replace("(사망자) ", "")
