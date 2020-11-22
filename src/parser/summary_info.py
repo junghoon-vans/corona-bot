9d4bfc765d95a8f5874661416302d2f954ee6b3b
Jeonghun-Ban <junghoon.ban@gmail.com>
2020-11-22 07:00:34 +0000

    feat: Add daily comfirmator

--- src/parser/summary_info.py
+++ src/parser/summary_info.py
@@ -3,6 +3,10 @@ import os
 
 # TODO 지역별 확진자
 
+def get_daily_num():
+    data = requests.get(os.environ['S3_BUCKET_URL']+"summary.json").json()
+    return data['update_date']+"\n일일확진자는 %s명입니다." % (int(data['domestic_num'])+int(data['oversea_num']))
+
 def get_confirmator_num():
     data = requests.get(os.environ['S3_BUCKET_URL']+"summary.json").json()
     return data['update_date']+"\n검사를 진행중인 인원은 %s명이며 확진환자는 %s명입니다.\n" % (data['check_num'], data['confirmator_num'])
